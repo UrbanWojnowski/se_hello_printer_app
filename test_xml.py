ab2b3c733957dbfcb61724771f982aebf3d640ac
UrbanWojnowski <ugwojnowski@gmail.com>
2020-02-23 14:00:37 +0100

    XML python

---
+++ test_xml.py
@@ -0,0 +1,11 @@
+import xml.etree.cElementTree as ET
+
+
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+
+s = ET.tostring(root)
+print(s)
