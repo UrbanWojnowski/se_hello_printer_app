d5a3716dda358767f347815f32f03d8296dc735f
UrbanWojnowski <ugwojnowski@gmail.com>
2020-02-23 13:24:31 +0100

    Dalej

--- hello_world/views.py
+++ hello_world/views.py
@@ -12,6 +12,13 @@ def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
+    name = request.args.get('name')
+    if not output:
+        output = PLAIN
+    elif not name:
+        name = PLAIN
+    if not name:
+        name = moje_imie
     return get_formatted(msg, moje_imie,
                          output.lower())
 
